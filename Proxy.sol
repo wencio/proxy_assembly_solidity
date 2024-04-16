13f4d122212d5242c4911eec7549e0be1099b298
wencio daniel martinez <97320103+wencio@users.noreply.github.com>
2024-04-15 19:36:00 -0500

    Update Proxy.sol

--- Proxy.sol
+++ Proxy.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.9;
+pragma solidity ^0.8.0;
 
 contract Proxy {
   address admin;
@@ -15,15 +15,15 @@ contract Proxy {
   /**
   * This function will return whatever the implementation call returns
   */
-  function () payable public {
+  fallback() payable public {
     require(implementation != address(0));
     address impl = implementation;
 
     assembly {
       let ptr := mload(0x40)
-      calldatacopy(ptr, 0, calldatasize)
-      let result := delegatecall(gas, impl, ptr, calldatasize, 0, 0)
-      let size := returndatasize
+      calldatacopy(ptr, 0, calldatasize())
+      let result := delegatecall(gas(), impl, ptr, calldatasize(), 0, 0)
+      let size := returndatasize()
       returndatacopy(ptr, 0, size)
 
       switch result
